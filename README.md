804db6750194c357202acc9f4fd07750ee58e3dc
shreyesss <64736777+shreyesss@users.noreply.github.com>
2021-04-06 00:11:48 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,130 @@
+# InterIIT_2021
+Repository for the Bosch's Traffic Sign Recognition challenge 
+
+The entire pipleine has been integrated in our UI, however if there is a need all the deep learning modules can be called individually as follows:
+
+Usage:
+- Augmentations  
+We can get the augmentations config file (aug.json) from the UI, or you can manually provide it
+```
+from augmentations import *
+import json
+import os
+path_to_aug_json = "aug.json"
+
+with open(path_to_aug_json) as f:
+  data = json.load(f)
+
+train_data_root = "Train_dummy/"
+new_train_data_root = "Train_dummy_augmented/"
+os.makedirs(new_train_data_root,exist_ok=True)
+
+generate_augented_dataset(data, train_data_root, new_train_data_root)
+```
+
+- Training our model  
+For training the model and logging the results for our UI and the MLFlow server
+
+```
+new_train_data_root = "Train_dummy_augmented/"
+test_data_root = "Test_dataset_48_classes"
+
+run_id = 'a56e04bce6fc41949f03f187221be156'
+model_path,run_id = train_classifier(new_train_data_root, test_data_root, run_id)
+```
+
+- Model evaluation  
+Computing evaluation metrics and logging them
+```
+from model_eval import *
+
+test_data_root = "Test_dummy/"
+model_path = "latest_model"
+model_eval_fns(test_data_root, model_path, classes, run_id)
+```
+
+- Model visulalization  
+Model visulalization techniques such as occlusion sensitivity maps, activation maps,etc
+```
+from model_viz import *
+test_data_root = "Train_dummy/"
+model_path = "final_model_test.h5"
+
+run_id = 'a56e04bce6fc41949f03f187221be156'
+viz_classes = ['2','5'] 
+
+visualize_main(test_data_root, model_path, run_id, viz_classes)
+```
+
+Our classes (including 5 addditional classes) are
+
+    0 : "Speed limit 20km/h",
+    1 : "Speed limit 30km/h",
+    2 : "Speed limit 50km/h",
+    3 : "Speed limit 60km/h",
+    4 : "Speed limit 70km/h",
+    5 : "Speed limit 80km/h",
+    6 : "End of speed limit 80km/h",
+    7 : "Speed limit 100km/h",
+    8 : "Speed limit 120km/h",
+    9 : "No passing",
+    10 : "No passing veh over 3.5 tons",
+    11 : "Right-of-way at intersection",
+    12 : "Priority road",
+    13 : "Yield",
+    14 : "Stop",
+    15 : "No vehicles",
+    16 : "Veh over 3.5 tons prohibited",
+    17 : "No entry",
+    18 : "General caution",
+    19 : "Dangerous curve left",
+    20 : "Dangerous curve right",
+    21 : "Double curve",
+    22 : "Bumpy road",
+    23 : "Slippery road",
+    24 : "Road narrows on the right",
+    25 : "Road work",
+    26 : "Traffic signals",
+    27 : "Pedestrians",
+    28 : "Children crossing",
+    29 : "Bicycles crossing",
+    30 : "Beware of ice/snow",
+    31 : "Wild animals crossing",
+    32 : "End speed passing limits",
+    33 : "Turn right ahead",
+    34 : "Turn left ahead",
+    35 : "Ahead only",
+    36 : "Go straight or right",
+    37 : "Go straight or left",
+    38 : "Keep right",
+    39 : "Keep left",
+    40 : "Roundabout mandatory",
+    41 : "End of no passing",
+    42 : "End no passing veh over 3.5 tons",
+    43 : "Parking",
+    44 : "No parking",
+    45 : "speed bump hump",
+    46 : "No Right",
+    47 : "Priority to",
+
+Apart from our own custom UI, we are also leveraging MLflow which is an open source platform to manage the ML lifecycle, including experimentation, reproducibility, deployment, and a central model registry.
+
+To access the MLFlow UI, you can run in the root folder
+```
+  mlflow ui 
+```
+
+To start an mlflow-server manually with a GCP backend and SQLite database
+```
+mlflow server \
+--backend-store-uri sqlite:///mlflow.db \
+--default-artifact-root gs://mlflow_artifacts_storage/artifacts/ \
+--host 0.0.0.0
+```
+For local run folder with SQLite database
+```
+mlflow server --backend-store-uri sqlite:///mydb.sqlite \
+  --default-artifact-root ./mlruns \
+  --port 8000
+```
+Use `mlflow.set_tracking_uri("https://35.198.166.98:5000") ` in code to log to a remote mlflow server
---
+++ server.py
@@ -0,0 +1,250 @@
+import os
+import warnings
+import argparse
+import yaml
+import csv
+import random
+import string
+import shutil
+import json
+import base64
+
+from flask           import Flask, request
+from flask_cors      import CORS
+from flask_socketio  import SocketIO, emit, disconnect
+
+from model_viz import visualize_main
+from augment import augmentation
+import model_eval as modelEval
+
+from model_eval import *
+import augmentations
+from start_train import train_classifier
+
+app = Flask(__name__, static_folder="./build", static_url_path="/")
+socketio = SocketIO(app, cors_allowed_origins='*', logger=True)
+
+dir_load = { "op":r'./data/', "preview":r'./data-pre/' }
+dir_save = { "op":r'./data-augmented/', "preview":r'./data-post/' }
+
+augpreview = augmentation(class_id=0, dict_transform=[], transform_vals=[], num_target_imgs=15, flag="preview")
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path>')
+def handler(path):
+    return app.send_static_file('index.html')
+
+@socketio.on('predict')
+def handle_input(images):
+    data = images.replace("data:image/jpeg;base64,", "")
+    data = data.replace("data:image/png;base64,", "")
+    imgdata = base64.b64decode(data)
+    with open("./data-eval/imageToSave.png", "wb") as fh:
+        fh.write(imgdata)
+    fh.close()
+    modelEval.predict_to_csv('./data-eval/imageToSave.png')
+    pieValues = []
+    headerList = ['Value', 'SignName']
+    with open('combined_predict.csv', 'w', newline='') as outcsv:
+        writer = csv.DictWriter(outcsv, fieldnames = ["Value", "SignName"])
+        writer.writeheader()
+
+        with open('predict_backend.csv', 'r', newline='') as incsv:
+            reader = csv.reader(incsv)
+            writer.writerows({'Value': row[0], 'SignName': row[1]} for row in reader)
+        incsv.close()
+    outcsv.close()
+    with open('combined_predict.csv') as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for index, row in enumerate(reader):
+            pieValues.append({'name':row['SignName'],'value':row['Value'],'title':'Graph 1'})
+    file.close()
+    return emit('predict',pieValues)
+
+@socketio.on('results')
+def handle_input(results):
+    r = open("./run_latest/conf_mat.png", 'rb')
+    pca = open("./run_latest/pca_analysis.png", 'rb')
+    matrixValues=[]
+
+    with open('./run_latest/combined_classification.csv', 'w', newline='') as outcsv:
+        writer = csv.DictWriter(outcsv, fieldnames = ["SignName","precision","recall","f1-score","support"])
+        writer.writeheader()
+
+        with open('./run_latest/classification_report.csv', 'r', newline='') as incsv:
+            reader = csv.reader(incsv)
+            writer.writerows({'SignName': row[0],'precision': row[1],"recall": row[2],"f1-score": row[3],"support": row[4]} for row in reader)
+        incsv.close()
+    outcsv.close()
+    with open('./run_latest/combined_classification.csv') as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for index, row in enumerate(reader):
+            matrixValues.append({'SignName':row['SignName'],'precision':row['precision'],'recall':row['recall'],'f1_score':row['f1-score'],'support':row['support']})
+
+    file.close()
+    pca_analysis=pca.read()
+    data = r.read()
+    train = open("./run_latest/finetune_train_acc.png",'rb')
+    finetune_acc = train.read()
+    train.close()
+    train = open('./run_latest/finetune_train_loss.png','rb')
+    finetune_loss = train.read()
+    train.close()
+    train = open('./run_latest/full_train_loss.png','rb')
+    full_loss = train.read()
+    train.close()
+    train = open('./run_latest/full_train_loss.png','rb')
+    full_acc = train.read()
+    train.close()
+    emit('results',{"img": data,"matrix": matrixValues,"pca": pca_analysis,'finetune_acc': finetune_acc,'finetune_loss': finetune_loss,'full_loss': full_loss,'full_acc': full_acc})
+    r.close()
+    pca.close()
+
+@socketio.on('visualize')
+def handle_input(results):
+
+    train = open("run_latest/interm_outputs/2/block1a_activation.jpg",'rb')
+    block_1a = train.read()
+    train.close()
+    train = open('run_latest/interm_outputs/2/block2a_activation.jpg','rb')
+    block_2a = train.read()
+    train.close()
+    train = open("run_latest/interm_outputs/5/block1a_activation.jpg",'rb')
+    block_51a = train.read()
+    train.close()
+    train = open('run_latest/interm_outputs/5/block2a_activation.jpg','rb')
+    block_52a = train.read()
+    train.close()
+    train = open("run_latest/interm_outputs/2/block1a_activation.jpg",'rb')
+    block_1a = train.read()
+    train.close()
+    train = open('run_latest/interm_outputs/2/block2a_activation.jpg','rb')
+    block_2a = train.read()
+    train.close()
+    train = open('run_latest/occ_maps/occ_map_2.png','rb')
+    occ_2 = train.read()
+    train.close()
+    train = open('run_latest/occ_maps/occ_map_5.png','rb')
+    occ_5 = train.read()
+    train.close()
+    emit('visualize',{'occ_2': occ_2,'occ_5': occ_5,'block_1a': block_1a,'block_2a': block_2a,'block_51a': block_51a,'block_52a': block_52a})
+
+@socketio.on('dirlist')
+def handle_input():
+    classnames = []
+    sublist = os.listdir(dir_load["op"])
+    with open('signnames.csv') as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for index, row in enumerate(reader):
+            classnames.append({'dir':index,'name':row['SignName'], 'num':len(os.listdir(dir_load["op"]+sublist[index]))})
+    return emit('dirlist', classnames)        
+
+@socketio.on('augsetdir')
+def handle_input(dirlist):
+    c = 15
+
+    for files in os.listdir(dir_load["preview"]):
+        path = os.path.join(dir_load["preview"], files)
+        os.remove(path)
+
+    while c > 0:
+        l = 0
+        for i in dirlist:
+            direc = os.path.join(dir_load["op"], str(i))
+            direcl = os.listdir(direc)
+            direcl.sort()
+
+            shutil.copy(direc + "/" + direcl[l], dir_load["preview"])
+            c -= 1
+            l += 1
+            if c <= 0:
+                break
+
+@socketio.on('augpreview')
+def handle_input(transform):
+    if transform :
+        augpreview.load_data()
+        augpreview.setup_pipeline(transform)
+        augpreview.augment(save=True)
+        
+        res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
+        direc = os.listdir(dir_save["preview"])
+        direc.sort()
+        
+        for i in direc:
+            r = open(dir_save["preview"] + i, 'rb')
+            data = r.read()
+            emit('augpreview',{"c" : res, "img": data})
+            r.close()
+            os.remove(dir_save["preview"] + i)
+    else:
+        res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
+        direc = os.listdir(dir_load["preview"])
+        direc.sort()
+
+        for i in direc:
+            r = open(dir_load["preview"] + i, 'rb')
+            data = r.read()
+            emit('augpreview',{"c" : res, "img": data})
+            r.close()
+    return 1
+
+@socketio.on('augment')
+def handle_input(data):
+    dump = open(data['file'], "a")
+    json.dump(data['data'], dump)
+    dump.close()
+
+@socketio.on('train')
+def handle_input(optionsDict):
+    path_to_aug_json = "aug.json"
+    path_to_defaug_json = "defaug.json"
+
+    with open(path_to_aug_json) as f:
+        aug = json.load(f)
+
+    with open(path_to_defaug_json) as f:
+        defaug = json.load(f)
+
+    train_data_root = "train/"
+    new_train_data_root = "train_augmented/"
+    test_data_root = "test/"
+    run_id = 'a56e04bce6fc41949f03f187221be156'
+
+    os.makedirs(new_train_data_root,exist_ok=True)
+
+    weight_mapping = {
+        'Pretrained weights': 'pretrained',
+        'Best Weights': 'best_weights',
+        'From scratch': 'scratch'
+        }
+
+    model_mapping = {
+        'EfficientNet': 'efficientnet',
+        '2 Layer ConvNet': '2layer_conv',
+        'ResNet50': 'resnet'
+        }
+
+    # Choosing augmentation
+    if optionsDict['aug'] == 'Configured params':
+        augmentations.generate_augented_dataset(aug, train_data_root, new_train_data_root)
+    elif optionsDict['aug'] == 'Default params':
+        augmentations.generate_augented_dataset(defaug, train_data_root, new_train_data_root)
+    elif optionsDict['aug'] == 'No augmentation':
+        new_train_data_root = train_data_root
+
+    model_path,run_id = train_classifier(new_train_data_root, test_data_root, train_type=weight_mapping[optionsDict['weights']], cnn_model=model_mapping[optionsDict['model']])
+
+    # ## Model evaluation
+    model_path = "final_model_test.h5"
+    model_eval_fns(test_data_root, model_path, run_id)
+
+    # ##Model visulalization
+    viz_classes = ['2','5']
+    visualize_main(test_data_root, model_path, run_id, viz_classes)
+    return emit('train', True)
+
+if __name__ == '__main__':
+    app.config['SECRET_KEY'] = 'BlaBlaBla'
+    CORS(app)
+    socketio.run(app, debug=True)
